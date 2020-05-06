5b7d4c72146110f18f1e26892b075710f7128dfb
luca.campli <campli.luca@gmail.com>
2020-05-06 18:53:59 -0300

    refactor tests

--- src/Autocomplete.test.js
+++ src/Autocomplete.test.js
@@ -5,42 +5,53 @@ import Autocomplete from "./Autocomplete";
 import fetchCountries from "./fetchCountries";
 import countries from "./countries.json";
 
+const setup = (placeholder = "Click me to see suggestions") => {
+  const utils = render(
+    placeholder ? (
+      <Autocomplete
+        fetchSuggestions={fetchCountries}
+        placeholder={placeholder}
+      />
+    ) : (
+      <Autocomplete fetchSuggestions={fetchCountries} />
+    )
+  );
+  const inputElement = utils.getByPlaceholderText(placeholder);
+  return {
+    ...utils,
+    inputElement,
+  };
+};
 describe("Autocomplete component", () => {
   it("renders properly", () => {
-    const { getByPlaceholderText } = render(
-      <Autocomplete fetchSuggestions={fetchCountries} />
-    );
-    const inputElement = getByPlaceholderText("Click me to see suggestions");
+    const { inputElement } = setup();
     expect(inputElement).toBeInTheDocument();
   });
 
   it("shows all the results when input is focused for the first time", async () => {
-    const { findAllByTestId, getByPlaceholderText } = render(
-      <Autocomplete
-        fetchSuggestions={fetchCountries}
-        placeholder="Country Name"
-      />
-    );
-    getByPlaceholderText("Country Name").focus();
+    const { findAllByTestId, inputElement } = setup("Country Name");
+    inputElement.focus();
     const suggestions = await findAllByTestId("suggestion");
 
     expect(suggestions.length).toBe(countries.length);
   });
 
   it("shows proper results when the user types", async () => {
-    const { findByText, getByPlaceholderText } = render(
-      <Autocomplete
-        fetchSuggestions={fetchCountries}
-        placeholder="Country Name"
-      />
-    );
-    const inputElement = getByPlaceholderText("Country Name");
+    const { findByText, inputElement } = setup("Country Name");
+
     fireEvent.change(inputElement, { target: { value: "ita" } });
 
     // no need to expect, findByText throws if it doesn't find any result
-    findByText("Italy");
+    await findByText("Ita");
   });
 
-  it("shows a user friendly message when no suggestions are available", async () => {});
+  it("shows a user friendly message when no suggestions are available", async () => {
+    const { findByText, inputElement } = setup("Country Name");
+
+    fireEvent.change(inputElement, { target: { value: "Not a country" } });
+
+    // no need to expect, findByText throws if it doesn't find any result
+    await findByText("No suggestions are available");
+  });
   it("shows a user friendly error message when fetch rejects", async () => {});
 });
